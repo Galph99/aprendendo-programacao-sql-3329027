31498a65f1937b1b9e12eff23064b4de609e4f44
Galph99 <163342837+Galph99@users.noreply.github.com>
2025-06-12 17:04:11 +0000

    RefinandoEx

--- exercicios/4-refinando-consulta/1-filtro.sql
+++ exercicios/4-refinando-consulta/1-filtro.sql
@@ -1,1 +1,8 @@
 -- Crie uma consulta para realizar inner join com as tabelas "tracks", "albums" e "artists". Além disso, apresente apenas as músicas do artista que contenha "Nação" no nome e título do álbum que não seja "Da Lama Ao Caos"
+-- artista Nação album <> Da Lama Ao Caos
+SELECT *
+FROM
+tracks
+INNER JOIN albums ON tracks.AlbumId = albums.AlbumId
+INNER JOIN artists ON artists.ArtistId = albums.ArtistId
+WHERE artists.Name LIKE '%Nação%' AND albums.Title != 'Da Lama Ao Caos';
--- exercicios/4-refinando-consulta/2-group-by-e-having.sql
+++ exercicios/4-refinando-consulta/2-group-by-e-having.sql
@@ -1,7 +1,32 @@
 -- Crie uma consulta usando as tabelas "invoices" e "customers" cujo o resultado seja o total de gastos por cliente
-
+SELECT 
+invoices.CustomerId, 
+customers.FirstName || ' ' || customers.LastName AS NomeCliente,
+SUM(invoices.Total), COUNT(invoices.Total)
+FROM 
+invoices
+INNER JOIN customers ON customers.CustomerId = invoices.CustomerId
+GROUP BY  invoices.CustomerId;
 
 -- Na consulta anterior, insira um filtro para que apresente apenas os clientes que gastaram mais de R$30
+WITH consulta AS
+(
+SELECT 
+invoices.CustomerId, 
+customers.FirstName || ' ' || customers.LastName AS NomeCliente,
+SUM(invoices.Total) AS TotalCliente, 
+COUNT(invoices.Total) AS QtdVendas,
+MAX(invoices.Total)
+FROM 
+invoices
+INNER JOIN customers ON customers.CustomerId = invoices.CustomerId
+GROUP BY  invoices.CustomerId
+)
+
+SELECT *
+FROM
+consulta  
+WHERE TotalCliente > 30;
 
 
 -- Na consulta anterior, insira uma coluna que informe a compra de maior valor feita por cada cliente
